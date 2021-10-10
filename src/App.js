bda155b20b728c800db10a3a5a0faccf45b3b691
Ash Midgley <ashleymidgley@protonmail.com>
2021-10-11 10:14:24 +1300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { SVGMap } from "@geobuff/svg-map";
+import { WorldCountries } from "@geobuff/svg-maps";
+
+const mapStyle = {
+  height: "80vh",
+  minWidth: "100%",
+  fill: "#6dca94",
+  margin: "12px",
+};
 
 function App() {
+  const [map, setMap] = React.useState(WorldCountries);
+  const [target, setTarget] = React.useState("");
+
+  const mouseOver = (event) => {
+    const name = event.currentTarget.getAttribute("name");
+    setMap({
+      ...map,
+      paths: map.paths
+        .map((x) => {
+          if (x.name.toLowerCase() === name.toLowerCase()) {
+            x.style = { "fill": "red" };
+          };
+          return x;
+        })
+    });
+  };
+
+  const mouseMove = (event) => {
+    setTarget(event.currentTarget.getAttribute("name"));
+  };
+
+  const onMouseOut = (event) => {
+    const name = event.currentTarget.getAttribute("name");
+    setMap({
+      ...map,
+      paths: map.paths
+        .map((x) => {
+          if (x.name.toLowerCase() === name.toLowerCase()) {
+            x.style = {};
+          };
+          return x;
+        })
+    });
+    setTarget("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        Moving across {target ? <p>{target}</p> : <p>...</p>}<p/>
+      </p>
+      <SVGMap map={map} mapStyle={mapStyle} onPathMouseOver={mouseOver} onPathMouseMove={mouseMove} onPathMouseOut={onMouseOut}/>
     </div>
   );
 }
